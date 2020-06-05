faee1016faf0d75a2ffc08464e6c3d16371c6f0c
Henrique <usuario@MacBook-Pro-de-Usuario.local>
2020-06-04 22:03:49 -0300

    usabilidade do input validando cadastro e exiindo erros

--- go-barber-web/src/pages/SignUp/index.tsx
+++ go-barber-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import LogoImg from '../../assets/logo.svg';
 
@@ -10,9 +13,28 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'No Mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
@@ -20,11 +42,11 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={LogoImg} width="170" alt="GoBarber" />
 
-        <Form initialData={{ name: 'John' }} onSubmit={handleSubmit}>
+        <Form ref={formRef} initialData={{ name: '' }} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="text" icon={FiMail} placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input name="password" icon={FiLock} placeholder="Senha" />
 
           <Button type="submit">Cadastrar</Button>
